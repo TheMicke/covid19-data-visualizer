a686f9b1208a220d396fd3fe431bc625518c06da
Mikael Ojaskivi <mikaelojaskivi@Mikaels-MacBook-Pro.local>
2020-05-09 12:28:23 +0200

    basic setup done

---
+++ server/routes.js
@@ -0,0 +1,13 @@
+const { getCurrentData } = require('./controllers/fetchController');
+
+// The routes availible for the backend
+module.exports = function(app) {
+    app.get('/api/v1/', (req, res) => {
+        res.send('Hi there..!');
+    });
+    
+    app.get('/api/v1/current', (req, res) => {
+        res.send(getCurrentData());
+    });
+};
+
